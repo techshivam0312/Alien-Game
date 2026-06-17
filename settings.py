eacd9e6620a3b6f82c41e397c630868aed37750a
techshivam0312 <techshivam0312@gmail.com>
2026-06-18 00:49:28 +0530

    Update settings.py

--- settings.py
+++ settings.py
@@ -3,7 +3,7 @@ class Settings() :
 
     def __init__(self):
         """Initilize the game settings """
-        #Screen settings 
+        
         self.screen_width = 1200
         self.screen_height = 800
         self.bg_color = (0,0,0)
