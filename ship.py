d8d5e779cb7023312fc1695546c30016640879cc
techshivam0312 <techshivam0312@gmail.com>
2026-06-18 00:49:48 +0530

    Update ship.py

--- ship.py
+++ ship.py
@@ -8,13 +8,11 @@ class Ship() :
 
         self.screen = screen
 
-        #load  the ship images and get its rect
         self.images = pygame.image.load("images/ship2.jpg")
         self.images = pygame.transform.scale(self.images,(80,90))
         self.rect = self.images.get_rect()
         self.screen_rect = screen.get_rect()
 
-        #Start each new ship at te bottom center of the screen
         self.rect.centerx = self.screen_rect.centerx
         self.rect.bottom = self.screen_rect.bottom
 
